038f8ec08d8aac9da4d3b689ac2bc81543dedee9
Ryan <94179306+ChNoir@users.noreply.github.com>
2022-03-16 16:30:11 +0100

    Update poulailler.py

--- BTS poulailler/poulailler.py
+++ BTS poulailler/poulailler.py
@@ -3,23 +3,38 @@ import signal
 import time
 import paho.mqtt.client as mqtt
 
-#Definition du client mqtt
+#############################
+# Definition du client mqtt #
+#############################
+
 mqtt_client =  mqtt.Client () 
-#Definition du broker
+
+########################
+# Definition du broker #
+########################
+
 broker = '5.196.95.208' # broker publique
 port = 1883
-#Definition du domain et des topics
+
+######################################
+# Definition du domain et des topics #
+######################################
+
 domain = "BTSpoulailler2022/"
 pubTopic = ["code_alerte","batterie", "pong"]
-subTopic =  ["ouverture","ping","remove_poule","add_poule", "test"]
+subTopic =  ["ping","remove_poule","add_poule", "heures","reload"]
 
-#Definition du client mariadb
+################################
+# Definition du client mariadb #
+################################
 
 conn = mysql.connector.connect( host="localhost",  user="juju", password="12345", database="Poulailler")
 mysql_client = conn.cursor()
 
+##########################
+# Definisiton des clases #
+##########################
 
-#Definisiton des clases
 class struc_msg () :
     payload = ""
     topic = ""
@@ -44,13 +59,18 @@ class struc_queue () :
         else :
             return None
 
-#Definition des queue
-#
-list_msg = struc_queue()
+
+########################
+# Definition des queue #
+########################
+
+sub_msg = struc_queue()
 pub_msg = struc_queue()
 
-#Definition des Function
-#  
+###########################
+# Definition des Function #
+###########################
+
 def on_connect (client , userdata , flags , rc) :
     print("Connecté")
     #définition des abonement
@@ -59,8 +79,8 @@ def on_connect (client , userdata , flags , rc) :
         
 
 def on_message(client, userdata, msg):
-    global list_msg
-    list_msg.add(msg)
+    global sub_msg
+    sub_msg.add(msg)
 
     
 def on_disconnect(client, userdata,rc):
@@ -80,39 +100,66 @@ def handler(signum, frame):
 
 signal.signal(signal.SIGINT, handler)
 
+########################
+# Definition du client #
+########################
 
-#Definition du client
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 mqtt_client.on_disconnect = on_disconnect
 mqtt_client.connect(broker,port)
 mqtt_client.loop_start ()
-#Loop
+
+########
+# Loop #
+########
+
 while True :  
-    time.sleep(0.1)
+    time.sleep(1)
     ################
     # MQTT get msg #
     ################
-    msg = list_msg.pop()
-    if msg != None :
+    sub = sub_msg.pop()
+    if sub != None :
         #Recupere un message de la centrale
         global payload ,topic
-        payload = str(msg.payload.decode("utf-8"))
-        topic = msg.topic.replace(domain,"")
+        payload = str(sub.payload.decode("utf-8"))
+        topic = sub.topic.replace(domain,"")
         print(f"[sub] massage : {payload} , topic : {topic}")
+        ####################
         if topic == "ping" :
             pub_msg.add(struc_msg("1","pong"))
-        if topic == "test" :
+        ####################
+        if topic == "add_poule" :
+            mysql_client.execute("INSERT INTO Poule (id_poule) VALUES (" + payload + ");")
+            conn.commit()
+            time.sleep(1)
             t = mysql_client.execute("SELECT * FROM Poule ;")
             print ( f"{mysql_client.fetchmany()}")
-        
+        ####################
+        if topic == "remove_poule" :
+            mysql_client.execute("DELETE FROM Poule WHERE id_poule = " + payload + ";")
+            conn.commit()
+        ####################
+        if topic == "heures" : 
+            i=1
+        ####################
+        if topic == "reload" :
+            mysql_client.execute("DELETE FROM Poule")
+            id_poule = payload.split(":")
+            for id in id_poule :
+                mysql_client.execute("INSERT INTO Poule (id_poule) VALUES (" + id + ");")
+            conn.commit()
+        ####################
+            
+
     #################
     # MQTT send msg #
     #################
-    msg = pub_msg.pop()
-    if msg != None :
-        print(f"[pub] massage : {msg.payload} , topic : {msg.topic}")
-        mqtt_client.publish(msg.topic ,msg.payload , msg.QoS)
+    pub = pub_msg.pop()
+    if  pub != None :
+        print(f"[pub] payload : { pub.payload} , topic : { pub.topic}")
+        mqtt_client.publish( pub.topic , pub.payload , pub.QoS)
     #autre programme
     
    
